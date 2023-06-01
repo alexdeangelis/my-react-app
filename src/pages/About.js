c4de61145788a8aa0305c6d6af75cc5cdf963a8f
Alex De Angelis <alexdeangelis86@googlemail.com>
2023-06-01 14:45:44 +0100

    including Lighthouse CI tests

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,16 @@
+// import axios from 'axios'
+
+// axios
+//   .get('https://photoslibrary.googleapis.com/v1/albums')
+//   .then((response) => {
+//     // Handle the response data
+//     console.log(response.data)
+//   })
+//   .catch((error) => {
+//     // Handle any errors
+//     console.error(error)
+//   })
+
 function About() {
   return <h1>This is the About Page</h1>
 }
