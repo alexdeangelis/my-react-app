5d640e3d8a5193e01024e0a3921554c8181aebbd
Alex De Angelis <alexdeangelis86@googlemail.com>
2023-06-02 11:50:42 +0100

    lighthouse testing update

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,11 +1,18 @@
 module.exports = {
   ci: {
     collect: {
-      url: '/about'
+      url: [
+        'http://127.0.0.1:3000'
+      ],
+      startServerCommand: "npm run build",
+      startServerReadyPattern: "Compiled successfully!",
+      numberOfRuns: 1
     },
     upload: {
       target: 'temporary-public-storage'
+    },
+    assert: {
+      preset: "lighthouse:recommended"
     }
   }
 }
-
